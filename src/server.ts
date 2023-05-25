5bc5a64ab8d41469925e313513186d7b9b184b22
Eduardo Schwanke <carvalhoe089@gmail.com>
2023-05-25 08:29:01 -0300

    aula 2

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import Fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { memoriesRoutes } from './routes/memories'
+import cors from '@fastify/cors'
 
 const app = Fastify({
   logger: true,
 })
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
+app.register(cors, {
+  origin: true,
 })
+app.register(memoriesRoutes)
 
 app.listen({ port: 3333 }, () => {
   return console.log('HTTP server is running on http://localhost:3333')
 })
+
+//01:18:00
